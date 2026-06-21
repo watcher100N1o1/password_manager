0d844cc7c86770545428b910e6ac283d9f5015df
flesh0of0time <nk@Neot.Mchn_spir.com>
2026-06-21 22:26:01 +0300

    json_working_part

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import random
 import json
 
+
 class PasswordRecord:
     def __init__(self, Service, Username, Password, CreatedAt):
         self.__record = {"Service": Service ,
@@ -11,3 +12,23 @@ class PasswordRecord:
     def get_rec(self):
         return self.__record
 
+
+def load_from_json_file(URL):
+    try:
+        with open(URL, "r", encoding="UTF-8") as file:
+            data = json.load(file)
+
+        return data
+    
+    except FileNotFoundError:
+
+        print("Ошибка: файл несуществует!")
+        print(f"Создание файла {URL}")
+
+        file = open(URL, "w", encoding="UTF-8")
+        file.close()
+
+
+def record_to_file(URL, base_lst):
+    with open(URL, "w", encoding="UTF-8") as file:
+        json.dump(base_lst, file, ensure_ascii=False, indent=4)
